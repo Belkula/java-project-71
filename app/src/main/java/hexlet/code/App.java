4b6c308c6cc5239ea146f020effc17645f78738f
Belkula <majin.td@mail.ru>
2024-03-06 23:48:22 +0300

    шаг 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,31 +6,31 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.security.MessageDigest;
 import java.util.concurrent.Callable;
 
-@Command(name = "checksum", mixinStandardHelpOptions = true, version = "checksum 4.0",
-         description = "Prints the checksum (SHA-256 by default) of a file to STDOUT.")
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
 
-    @Parameters(index = "0", description = "The file whose checksum to calculate.")
-    private File file;
+    @Parameters(index = "0", description = "path to first file")
+    private File filepath1;
 
-    @Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
-    private String algorithm = "SHA-256";
+    @Parameters(index = "1", description = "path to second file")
+    private File filepath2;
 
-    @Override
-    public Integer call() throws Exception { 
-        byte[] fileContents = Files.readAllBytes(file.toPath());
-        byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
-        System.out.printf("%0" + (digest.length*2) + "x%n", new BigInteger(1, digest));
-        return 0;
-    }
+    @Option(names = {"-f", "--format"}, description = "[default: stylish]")
+    private String format = "stylish";
+
 
     public static void main(String... args) {
+        System.out.println("----------------------");
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        // логика сравнения
+        return 0;
     }
 }
