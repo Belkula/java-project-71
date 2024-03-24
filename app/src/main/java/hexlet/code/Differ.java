8390dedeafdc82bf0aad7ea09c4ef49953f65346
Belkula <majin.td@mail.ru>
2024-03-24 23:18:51 +0300

    изменён Differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -49,7 +49,7 @@ public class Differ {
     }
 
     public static String generate(String filePath1, String filePath2) throws Exception {   
-        return generate(filePath1, filePath2, "stylish")
+        return generate(filePath1, filePath2, "stylish");
     }
 
     public static String generate(String filePath1, String filePath2, String format) throws Exception {
